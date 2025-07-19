d4676c7fec817c2a21e8fa5c40dc484508e17954
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-07-19 17:31:22 +0000

    Adds backend continuous deployment workflow
    Implements a CD workflow triggered by successful CI runs.
    
    This workflow pulls the latest Docker image, stops and removes the old container, and runs a new container with the updated image.

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World hah! 2';
+    return 'Hello World - William the Devops';
   }
 }
