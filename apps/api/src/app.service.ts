504164edf9ab10335bd875dd5418ad6612c9a70d
Zerebel <86872654+Zerebel@users.noreply.github.com>
2025-07-19 16:41:18 +0000

    Updates Docker image and greeting
    Updates the Docker image build and push commands to use the Docker Hub username stored in secrets.
    
    Also, modifies the greeting message in the API.

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World hah!';
+    return 'Hello World hah! 2';
   }
 }
